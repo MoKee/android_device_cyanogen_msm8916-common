ce634bb66805c083893e56c7347a4f1ec3f5ee20
martincz <martincz@mokeedev.com>
2017-08-03 21:51:47 +0800

    msm8916-common: exclude a6000
    Change-Id: Ia474d66db44b23e3f2c6b669e8c3f94a760025c5

--- Android.mk
+++ Android.mk
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-ifeq ($(filter peach, $(TARGET_DEVICE)),)
+ifeq ($(filter peach a6000, $(TARGET_DEVICE)),)
 
 LOCAL_PATH := $(call my-dir)
 
