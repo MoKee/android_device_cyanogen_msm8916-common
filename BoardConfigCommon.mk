b0b699430a2d95f73a137740a3b20c0df7227aa5
GuaiYiHu <guaiyihu@foxmail.com>
2019-09-19 12:54:35 +0800

    [DNM]msm8916-common: Drop sepolicy
    Change-Id: If003dc54c575c46c58ac5c231650fca86eeb0446

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -126,11 +126,6 @@ TARGET_RECOVERY_UPDATER_LIBS := librecovery_updater_cm
 # Releasetools
 TARGET_RELEASETOOLS_EXTENSIONS := $(PLATFORM_PATH)
 
-# SELinux
-include device/qcom/sepolicy-legacy/sepolicy.mk
-BOARD_SEPOLICY_DIRS += \
-    $(PLATFORM_PATH)/sepolicy
-
 # Shims
 TARGET_LD_SHIM_LIBS := \
     /system/vendor/lib64/libflp.so|libshims_flp.so \
